770f5786616e87cb3d6b3252b4f538a2fd913572
kamoM312 <kamogelomalema1@gmail..com>
2024-08-01 20:25:18 +0200

    Created file. Add custom admincs and adminjs links. Included admin_header.php in html body.

---
+++ admin_home.php
@@ -0,0 +1,39 @@
+<?php
+
+include 'config.php';
+
+session_start();
+
+$admin_id = $_SESSION['admin_id'];
+
+if(!isset($admin_id)) {
+	header('location:login.php');
+}
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title> Administration Console</title>
+
+	<!-- custom css -->
+	<link rel="stylesheet" href="css/admin_style.css">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+<body>
+
+<!-- including admin_header.php file -->
+<?php include 'admin_header.php'; ?>
+
+
+
+
+ <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+<!-- including js file -->
+<script src="js/admin_script.js"></script>
+
+</body>
+</html>
