c333e5a282c1d7b37fb3f94d69e0aa679005c3cd
kamoM312 <kamogelomalema1@gmail..com>
2024-08-06 19:15:28 +0200

    Reindented lines. Changed delete button text

--- admin_users.php
+++ admin_users.php
@@ -8,7 +8,7 @@ $admin_id = $_SESSION['admin_id'];
 
 if(!isset($admin_id)) {
 	header('location:login.php');
-}
+};
 
 // delete users
 if(isset($_GET['delete'])){
@@ -33,59 +33,36 @@ if(isset($_GET['delete'])){
 </head>
 <body>
 
-<?php include 'admin_header.php'; ?>
+	<?php include 'admin_header.php'; ?>
 
-<section class="users">
-	<h1 class="title">User Accounts</h1>
-	
-	<div class="box-container">
-		
-		<?php 
+	<section class="users">
+		<h1 class="title">User Accounts</h1>
+		 
+		<div class="box-container">
 
-		$select_users = mysqli_query($conn, "SELECT * FROM Users") or die('Query Unsuccessful!');
-		while($fetch_users = mysqli_fetch_assoc($select_users)){
+			<?php 
+
+			$select_users = mysqli_query($conn, "SELECT * FROM Users") or die('Query Unsuccessful!');
+			while($fetch_users = mysqli_fetch_assoc($select_users)){
+
+				?>
+				<div class="box">
+
+					<p>Username : <span><?php echo $fetch_users['Name']; ?></span></p>
+					<p>Email : <span><?php echo $fetch_users['Email']; ?></span></p>
+					<!-- add code to have admin be a different color -->
+					<p>User type : <span><?php echo $fetch_users['UserType']; ?></span></p>
+					<a href="admin_users.php?delete=<?php echo $fetch_users['ID']; ?>" onclick="return confirm('Delete this user?');" class="delete-btn">Delete User</a>
+
+				</div>
+				<?php
+			};
 
 			?>
-			<div class="box">
-				
-				<p>Username : <span><?php echo $fetch_users['Name']; ?></span></p>
-				<p>Email : <span><?php echo $fetch_users['Email']; ?></span></p>
-				<!-- add code to have admin be a different color -->
-				<p>User type : <span><?php echo $fetch_users['UserType']; ?></span></p>
-				<a href="admin_users.php?delete=<?php echo $fetch_users['ID']; ?>" onclick="return confirm('Delete this user?');" class="delete-btn">Delete</a>
 
-			</div>
-			<?php
-				};
+		</div>
 
-		?>
-
-	</div>
-
-</section>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+	</section>
 
 
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script> 
