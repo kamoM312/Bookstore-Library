f74f1568ef7ad193008090524d4e290cd50ae2b2
kamoM312 <kamogelomalema1@gmail..com>
2024-08-10 20:39:43 +0200

    Created file. Contains navbars for the user pages. Navbars include: Logo, buttons, sign in and sign up links

---
+++ header.php
@@ -0,0 +1,44 @@
+<header class="header">
+	
+	<div class="header-1">
+		<div class="flex">
+			<div class="logo-nav">
+				<a href="home.php" class="logo">Book<span>Worms.</span></a>	
+			</div>
+			<p><a href="register.php">Sign up</a> | <a href="login.php">Sign in</a></p>
+		</div>
+	</div>
+	<div class="header-2">
+		<div class="flex">
+			<nav class="navbar">
+				<a href="home.php">home</a>
+				<a href="about.php">about</a>
+				<a href="shop.php">shop</a>
+				<a href="contact.php">contact</a>
+				<a href="orders.php">orders</a>
+			</nav>
+
+			<div class="icons">
+				<div id="menu-btn" class="fas fa-bars"></div>
+				<a href="search_page.php" class="fas fa-search"></a>
+				<div id="user-btn" class="fas fa-user"></div>
+
+				<?php 
+					$select_cart_number = mysqli_query($conn, "SELECT * FROM Cart WHERE USER_ID = '$user_id'") or die('Query Unsuccessful!');
+					$cart_rows_number = mysqli_num_rows($select_cart_number);
+				?>
+
+				<a href="cart.php"><i class="fas fa-shopping-cart"></i><span>(<?php echo $cart_rows_number; ?>)</span></a>
+			</div>
+
+			<div class="user-box">
+				
+				<p>username : <span><?php echo $_SESSION['user_name']; ?></span></p>
+				<p>email : <span><?php echo $_SESSION['user_email']; ?></span></p>
+				<a href="logout.php" class="delete-btn">LOGOUT</a>
+
+			</div>
+		</div>
+	</div>
+
+</header>
