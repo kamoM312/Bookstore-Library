d9bbb20b0e5cf3caf5532cf780798d367b372c02
kamoM312 <kamogelomalema1@gmail..com>
2024-08-23 11:38:17 +0200

    added user button and menu button functionality

--- js/admin_script.js
+++ js/admin_script.js
@@ -1,4 +1,16 @@
 // Update form cancel button action
 document.querySelector('#close-update').onclick = () =>{
 	document.querySelector('.edit-product-form').style.display = 'none';
+	window.location.href = 'admin_products.php';
+}
+
+let navbar = document.querySelector('.header .navbar');
+let accountBox = document.querySelector('.header .account-box');
+
+document.querySelector('#menu-btn').onclick = () => {
+	navbar.classList.toggle('active');
+}
+
+document.querySelector('#user-btn').onclick = () => {
+	accountBox.classList.toggle('active');
 }
