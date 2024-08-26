686a2673ca8b3265e81308196e1f9a4bd43b12d5
kamoM312 <kamogelomalema1@gmail..com>
2024-08-26 11:31:45 +0200

    fixed heading section h3 text

--- shopFantasySciFi.php
+++ shopFantasySciFi.php
@@ -54,7 +54,7 @@
 
  	<div class="heading">
 
- 		<h3>shop</h3>
+ 		<h3>fantasy & sci-fi</h3>
  		<p><a href="home_page.php">home</a> / <a href="shop.php">shop</a> / fantasy & sci-fi </p>
 
  	</div>
