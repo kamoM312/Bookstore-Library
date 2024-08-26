aa5bef3548e83fd508f4b88b0d66ded73016cea1
kamoM312 <kamogelomalema1@gmail..com>
2024-08-26 10:43:25 +0200

    changed navbar2 shop link into a dropdown btn with links

--- header.php
+++ header.php
@@ -27,7 +27,20 @@ if(isset($message)) {
 			<nav class="navbar">
 				<a href="home_page.php">home</a>
 				<a href="about.php">about</a>
-				<a href="shop.php">shop</a>
+				<div class="dropdown">
+					<button class="dropbtn">shop
+						<i class="fa fa-caret-down"></i>
+					</button>
+					<div class="dropdown-content">
+						<a href="shop.php">all books</a>
+						<a href="shopNew.php">new arrivals</a>
+						<a href="shopSale.php">sale</a>
+						<a href="shopFantasySciFi.php">fantasy & sci-fi</a>
+						<a href="shopCrimeThriller.php">crime & thriller</a>
+						<a href="shopHistorical.php">historical</a>
+						<a href="shopHorror.php">horror</a>
+					</div>
+				</div> 
 				<a href="contact.php">contact</a>
 				<a href="orders.php">orders</a>
 			</nav>
