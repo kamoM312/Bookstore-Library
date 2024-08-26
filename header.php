a15abdfb5bebbf85e81255cc1d289ba842286a4f
kamoM312 <kamogelomalema1@gmail..com>
2024-08-26 12:48:36 +0200

    added text to accompany icons

--- header.php
+++ header.php
@@ -46,16 +46,16 @@ if(isset($message)) {
 			</nav>
 
 			<div class="icons">
-				<div id="menu-btn" class="fas fa-bars"></div>
-				<a href="search_page.php" class="fas fa-search"></a>
-				<div id="user-btn" class="fas fa-user"></div>
+				<div id="menu-btn" class="fas fa-bars"> Menu |</div>
+				<a href="search_page.php" class="fas fa-search"> Search |</a>
+				<div id="user-btn" class="fas fa-user"> Account |</div>
 
 				<?php 
 					$select_cart_number = mysqli_query($conn, "SELECT * FROM Cart WHERE USER_ID = '$user_id'") or die('Query Unsuccessful!');
 					$cart_rows_number = mysqli_num_rows($select_cart_number);
 				?>
 
-				<a href="cart.php"><i class="fas fa-shopping-cart"></i><span>(<?php echo $cart_rows_number; ?>)</span></a>
+				<a href="cart.php"><i class="fas fa-shopping-cart"> Cart </i><span>(<?php echo $cart_rows_number; ?>)</span></a>
 			</div>
 
 			<div class="user-box">
