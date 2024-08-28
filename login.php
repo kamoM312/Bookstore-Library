972ef8a005c5dd949167b6f33a29731a8caa8b0f
kamoM312 <kamogelomalema1@gmail..com>
2024-08-28 14:42:47 +0200

    improved security: empty fields cannot be submitted, htmlspecial characters function and extra input cleaning function prevents cross site scripting

--- login.php
+++ login.php
@@ -3,14 +3,71 @@
 
 <?php
 
+ini_set('display_errors', 1);
+
 include 'config.php';
 
 session_start();
 
-	if(isset($_POST['submit'])){
+// 	if(isset($_POST['submit'])){
 
-	$email = mysqli_real_escape_string($conn, $_POST['email']);
-	$pass = mysqli_real_escape_string($conn, $_POST['password']);
+// 	$email = mysqli_real_escape_string($conn, $_POST['email']);
+// 	$pass = mysqli_real_escape_string($conn, $_POST['password']);
+	
+// 	$select_users = mysqli_query($conn, "SELECT * FROM Users WHERE Email = '$email' AND Password = '$pass'") or die('query failed');
+
+// 	if(mysqli_num_rows($select_users) > 0) {
+// 		$row = mysqli_fetch_assoc($select_users);
+
+// 		if($row['UserType'] == 'admin'){
+
+// 			$_SESSION['admin_name'] = $row['Name'];
+// 			$_SESSION['admin_email'] = $row['Email'];
+// 			$_SESSION['admin_id'] = $row['ID'];
+
+// 			header('location:admin_home.php');
+// 		} elseif($row['UserType'] == 'user') {
+
+// 			$_SESSION['user_name'] = $row['Name'];
+// 			$_SESSION['user_email'] = $row['Email'];
+// 			$_SESSION['user_id'] = $row['ID'];
+			
+// 			header('location:home_page.php');
+// 		}
+// 	} else {
+// 		$message[] = 'Email or password combination is incorrect!';
+// 	}
+// }
+
+function test_input($data) {
+	$data = trim($data);
+	$data = stripslashes($data);
+	$data = htmlspecialchars($data);
+	return $data;
+}
+
+$email = $pass = "";
+$valid = "true";
+
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+	if (empty($_POST['email'])) {
+		$message[] = 'Email is required';
+		$valid = "false";
+
+	} else {
+		$email = test_input($_POST['email']);
+	}
+
+	if (empty($_POST['password'])) {
+		$message[] = 'Password is required';
+		$valid = "false";
+	} else {
+		$pass = test_input($_POST['password']);
+	}
+
+	if ($valid == "true"){
+		$email = mysqli_real_escape_string($conn, $email);
+	$pass = mysqli_real_escape_string($conn, $pass);
 	
 	$select_users = mysqli_query($conn, "SELECT * FROM Users WHERE Email = '$email' AND Password = '$pass'") or die('query failed');
 
@@ -35,9 +92,8 @@ session_start();
 	} else {
 		$message[] = 'Email or password combination is incorrect!';
 	}
+	}
 }
-
-
 
 ?>
 
@@ -74,7 +130,7 @@ if(isset($message)) {
 		<!-- registration form -->
 		<div class="form">
 			<!-- Adding required class attribute so form can only be submitted once required values are entered -->
-			<form action="" method="post">
+			<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
 				<h4>Login</h4>
 				<input type="email" name="email" placeholder="Email" required class="box">
 				<input type="password" name="password" placeholder="password" required class="box">
