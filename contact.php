06c52d10c548ef18c2260da5b40ef9cbbb386e8d
kamoM312 <kamogelomalema1@gmail..com>
2024-08-29 11:39:05 +0200

    File:contact.php  added form submission validation and input cleaning to prevent the submission of empty fields and to prevent cross site scripting

--- contact.php
+++ contact.php
@@ -1,99 +1,159 @@
  <?php
 
-include 'config.php';
+ ini_set('display_errors', 1);
 
-session_start();
+ include 'config.php';
 
-$user_id = $_SESSION['user_id'];
+ session_start();
 
-if(!isset($user_id)) {
-	header('location:login.php');
-}
+ $user_id = $_SESSION['user_id'];
 
-if(isset($_POST['send'])){
+ if(!isset($user_id)) {
+ 	header('location:login.php');
+ }
 
-	$name = mysqli_real_escape_string($conn, $_POST['name']);
-	$email = mysqli_real_escape_string($conn, $_POST['email']);
-	$number = $_POST['number'];
-	$msg = mysqli_real_escape_string($conn, $_POST['message']);
+// if(isset($_POST['send'])){
 
-	$select_message = mysqli_query($conn, "SELECT * FROM Message WHERE Name = '$name' AND Email = '$email' AND Number = '$number' AND Message = '$msg'") or die('Query Unsuccessful');
+// 	$name = mysqli_real_escape_string($conn, $_POST['name']);
+// 	$email = mysqli_real_escape_string($conn, $_POST['email']);
+// 	$number = $_POST['number'];
+// 	$msg = mysqli_real_escape_string($conn, $_POST['message']);
 
-	if(mysqli_num_rows($select_message) > 0){
-		$message[] = 'message has already been sent';
-	} else {
-		mysqli_query($conn, "INSERT INTO Message (User_ID, Name, Email, Number, Message) VALUES('$user_id', '$name', '$email', '$number', '$msg')") or die('Query Unsuccessful!');
-		$message[] = 'message was successfully sent!';
-	}
+// 	$select_message = mysqli_query($conn, "SELECT * FROM Message WHERE Name = '$name' AND Email = '$email' AND Number = '$number' AND Message = '$msg'") or die('Query Unsuccessful');
 
-}
+// 	if(mysqli_num_rows($select_message) > 0){
+// 		$message[] = 'message has already been sent';
+// 	} else {
+// 		mysqli_query($conn, "INSERT INTO Message (User_ID, Name, Email, Number, Message) VALUES('$user_id', '$name', '$email', '$number', '$msg')") or die('Query Unsuccessful!');
+// 		$message[] = 'message was successfully sent!';
+// 	}
 
-?>
+// }
 
-<!DOCTYPE html>
-<html>
-<head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>contact us</title>
+ function test_input($data) {
+ 	$data = trim($data);
+ 	$data = stripslashes($data);
+ 	$data = htmlspecialchars($data);
+ 	return $data;
+ }
 
-	<!-- Add icon library -->
-    <link rel="stylesheet" 
-          href=
-"https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css">
+ $valid = "true";
 
-<!-- custom css -->
-	<link rel="stylesheet" href="css/style.css">
-</head>
-<body>
+ if($_SERVER["REQUEST_METHOD"] == "POST"){
 
-</head>
-<body>
+ 	if (empty($_POST['name'])) {
+ 		$message[] = 'Name is required';
+ 		$valid = "false";
 
-	<?php include 'header.php'; ?>
+ 	} else {
+ 		$name = test_input($_POST['name']);
 
-<div class="heading">
+ 		if(!preg_match("/^[a-zA-Z-' ]*$/",$name)) {
+ 			$message[] = 'Name field only allows letters and white space.';
+ 			$valid = "false";
+ 		}
+ 	}
 
- 		<h3>contact us</h3>
- 		<p><a href="home_page.php">home</a> / contact </p>
+ 	if (empty($_POST['email'])) {
+ 		$message[] = 'Email is required';
+ 		$valid = "false";
+ 	} else {
+ 		$email = test_input($_POST['email']);
 
- 	</div>
+ 		if (!filter_var($email, FILTER_VALIDATE_EMAIL))  {
+ 			$message[] = 'Invalid email format.';
+ 			$valid = "false";
+ 		}
+ 	}
 
-<!-- contact form -->
+ 	if (empty($_POST['message'])) {
+ 		$valid = "false";
+ 	} else {
+ 		$msg = test_input($_POST['message']);
+ 	}
 
-<section class="contact">
+ 	if (empty($_POST['number'])) {
+ 		$valid = "false";
+ 	} else {
+ 		$number = test_input($_POST['number']);
+ 	}
 
-<div class="contact-textarea">
-	<h3>Contact us</h3>
-	<p>We would like to hear from you. Please send us a message by filling out the form below and we will get back with you shortly.</p>
-</div>
+ 	if ($valid == "true"){
+ 		$name = mysqli_real_escape_string($conn, $name);
+ 		$email = mysqli_real_escape_string($conn, $email);
+ 		$number = mysqli_real_escape_string($conn, $number);
+ 		$msg = mysqli_real_escape_string($conn, $msg);
 
-	<form method="post">
+ 		$select_message = mysqli_query($conn, "SELECT * FROM Message WHERE Name = '$name' AND Email = '$email' AND Number = '$number' AND Message = '$msg'") or die('Query Unsuccessful!');
 
-		<input class="box" type="text" name="name" placeholder="Enter your name..." required>
-		<input class="box" type="email" name="email" placeholder="Enter your email..." required>
-		<input class="box" type="number" name="number" placeholder="Enter your contact number..." required>
-		<textarea class="box" name="message" placeholder="Enter your message here..." cols="30" rows="10" required></textarea>
-		<input type="submit" name="send" value="send message" class="white-btn">
-
-	</form>
-
-</section>
-
-
-
-
-
-
-
- 
-
-
+ 		if(mysqli_num_rows($select_message) > 0){
+ 			$message[] = 'message has already been sent';
+ 		} else {
+ 			mysqli_query($conn, "INSERT INTO Message (User_ID, Name, Email, Number, Message) VALUES('$user_id', '$name', '$email', '$number', '$msg')") or die('Query Unsuccessful!');
+ 			$message[] = 'message was successfully sent!';
+ 		}
+ 	}
+ }
 
 
-	<?php include 'footer.php'; ?>
+ 	?>
 
-	<script src="js/script.js"></script>
+ 	<!DOCTYPE html>
+ 	<html>
+ 	<head>
+ 		<meta charset="utf-8">
+ 		<meta name="viewport" content="width=device-width, initial-scale=1">
+ 		<title>contact us</title>
 
-</body>
-</html>
+ 		<!-- Add icon library -->
+ 		<link rel="stylesheet" 
+ 		href=
+ 		"https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css">
+
+ 		<!-- custom css -->
+ 		<link rel="stylesheet" href="css/style.css">
+ 	</head>
+ 	<body>
+
+ 	</head>
+ 	<body>
+
+ 		<?php include 'header.php'; ?>
+
+ 		<div class="heading">
+
+ 			<h3>contact us</h3>
+ 			<p><a href="home_page.php">home</a> / contact </p>
+
+ 		</div>
+
+ 		<!-- contact form -->
+
+ 		<section class="contact">
+
+ 			<div class="contact-textarea">
+ 				<h3>Contact us</h3>
+ 				<p>We would like to hear from you. Please send us a message by filling out the form below and we will get back with you shortly.</p>
+ 			</div>
+
+ 			<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
+
+ 				<input class="box" type="text" name="name" placeholder="Enter your name..." required>
+ 				<input class="box" type="email" name="email" placeholder="Enter your email..." required>
+ 				<input class="box" type="number" name="number" placeholder="Enter your contact number..." required>
+ 				<textarea class="box" name="message" placeholder="Enter your message here..." cols="30" rows="10" required></textarea>
+ 				<div class="flex">
+ 					<input type="submit" name="send" value="send message" class="btn">
+ 				</div>
+
+
+ 			</form>
+
+ 		</section>
+
+ 		<?php include 'footer.php'; ?>
+
+ 		<script src="js/script.js"></script>
+
+ 	</body>
+ 	</html>
