4ed780921b7407a9cb700fa56771b0f94ff5d0ed
kamoM312 <kamogelomalema1@gmail..com>
2024-08-30 13:05:18 +0200

    made changes

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ let userBox = document.querySelector('.header .header-2 .flex .user-box');
 
 document.querySelector('#user-btn').onclick = () =>{
 	userBox.classList.toggle('active');
+	navbar.classList.remove('active');
 }
 
 // make nav button work
@@ -10,4 +11,19 @@ let navbar = document.querySelector('.header .header-2  .flex .navbar');
 
 document.querySelector('#menu-btn').onclick = () =>{
 	navbar.classList.toggle('active');
+	userBox.classList.remove('active');
+
 }
+
+// make navbar and userBox dissapear on scroll
+window.onscroll = () => {
+	userBox.classList.remove('active');
+	navbar.classList.remove('active');
+
+	if(window.scrollY > 60){
+		document.querySelector('.header .header-2').classList.add('active');
+	} else {
+		document.querySelector('.header .header-2').classList.remove('active');
+	}
+}
+
