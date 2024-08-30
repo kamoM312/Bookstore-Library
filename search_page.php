9b062d2f0ab46ec903eaaffea865aada8122ef47
kamoM312 <kamogelomalema1@gmail..com>
2024-08-30 09:13:50 +0200

    search_page.php: improved form security using htmlspecialcharacters function, self reference and input cleaning

--- search_page.php
+++ search_page.php
@@ -1,5 +1,7 @@
  <?php
 
+ ini_set('display_errors', 1);
+
  include 'config.php';
 
  session_start();
@@ -10,21 +12,98 @@
  	header('location:login.php');
  }
 
-  if(isset($_POST['add_to_cart'])){
+ //  if(isset($_POST['add_to_cart'])){
 
- 	$product_name = $_POST['product_name'];
- 	$product_price = $_POST['product_price'];
- 	$product_image = $_POST['product_image'];
- 	$product_quantity = $_POST['product_quantity'];
+ // 	$product_name = $_POST['product_name'];
+ // 	$product_price = $_POST['product_price'];
+ // 	$product_image = $_POST['product_image'];
+ // 	$product_quantity = $_POST['product_quantity'];
 
- 	$check_cart_numbers = mysqli_query($conn, "SELECT * FROM Cart WHERE Name = '$product_name' AND User_ID = '$user_id'") or die('Query Unsuccessful!');
+ // 	$check_cart_numbers = mysqli_query($conn, "SELECT * FROM Cart WHERE Name = '$product_name' AND User_ID = '$user_id'") or die('Query Unsuccessful!');
 
- 	if(mysqli_num_rows($check_cart_numbers) > 0) {
- 		$message[] = 'already added to cart';
+ // 	if(mysqli_num_rows($check_cart_numbers) > 0) {
+ // 		$message[] = 'already added to cart';
+ // 	} else {
+ // 		mysqli_query($conn, "INSERT INTO Cart (User_ID, Name, Price, Quantity, Image) VALUES('$user_id', '$product_name', '$product_price','$product_quantity', '$product_image')") or die('Query Unsuccessful!');
+ // 		$message[] = 'Product added to cart.';
+ // 	}
+
+ // }
+
+  function test_input($data) {
+ 	$data = trim($data);
+ 	$data = stripslashes($data);
+ 	$data = htmlspecialchars($data);
+ 	return $data;
+ }
+
+// $product_name = $product_author = $product_image = $product_price = $product_quantity = "";
+
+ $valid = "true";
+
+ if($_SERVER["REQUEST_METHOD"] == "POST") {
+
+ 	if(isset($_POST['add_to_cart'])){
+ 		if (empty($_POST['product_name'])) {
+ 			$valid = "false";
+ 		} else {
+ 			$product_name = test_input($_POST['product_name']);
+ 		}
+
+ 		if (empty($_POST['product_price'])) {
+ 			$valid = "false";
+ 		} else {
+ 			$product_price = test_input($_POST['product_price']);
+ 		}
+
+ 		if (empty($_POST['product_image'])) {
+ 			$valid = "false";
+ 		} else {
+ 			$product_image = test_input($_POST['product_image']);
+ 		}
+
+ 		if (empty($_POST['product_quantity'])) {
+ 			$valid = "false";
+ 		} else {
+ 			$product_quantity = test_input($_POST['product_quantity']);
+ 			if (!preg_match("/\d/", $product_quantity) || ($product_quantity < 1)) {
+ 				$message[] = "Product quantity should be a number equal to or greater than 1";
+ 				$valid = "false";
+ 			}
+ 		}
+
+ 	if (empty($_POST['product_author'])) {
+ 		$valid = "false";
  	} else {
- 		mysqli_query($conn, "INSERT INTO Cart (User_ID, Name, Price, Quantity, Image) VALUES('$user_id', '$product_name', '$product_price','$product_quantity', '$product_image')") or die('Query Unsuccessful!');
- 		$message[] = 'Product added to cart.';
+ 		$product_author = test_input($_POST['product_author']);
  	}
+
+ 	if ($valid == "true"){
+ 		$product_name = mysqli_real_escape_string($conn, $product_name);
+ 		$product_price = mysqli_real_escape_string($conn, $product_price);
+ 		$product_image = mysqli_real_escape_string($conn, $product_image);
+ 		$product_quantity = mysqli_real_escape_string($conn, $product_quantity);
+ 		$product_author = mysqli_real_escape_string($conn, $product_author);
+
+ 		$check_cart_numbers = mysqli_query($conn, "SELECT * FROM Cart WHERE Name = '$product_name' AND User_ID = '$user_id'") or die('Query Unsuccessful!');
+
+ 		if(mysqli_num_rows($check_cart_numbers) > 0) {
+ 			$message[] = 'already added to cart';
+ 		} else {
+ 			mysqli_query($conn, "INSERT INTO Cart (User_ID, Name, Price, Quantity, Image, Author) VALUES('$user_id', '$product_name', '$product_price','$product_quantity', '$product_image', '$product_author')") or die('Query Unsuccessful!');
+ 			$message[] = 'Product added to cart.';
+ 		}
+
+ 	} else {
+ 		$message[] = 'Product values invalid!';
+ 	}
+
+
+
+ 	}
+
+ 	
+
 
  }
 
@@ -61,8 +140,8 @@
 
  	<section class="search-form">
  		
- 		<form method="post">
- 			<input class="box" type="text" name="search" placeholder="search products...">
+ 		<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
+ 			<input class="box" type="text" name="search" placeholder="search by product name, author...">
  			<input class="btn" type="submit" name="submit" value="search">
  		</form>
 
@@ -74,21 +153,27 @@
  			
  			<?php  
 
+ 			if($_SERVER["REQUEST_METHOD"] == "POST") {
+
  			if(isset($_POST['submit'])){
- 				$search_item = $_POST['search'];
- 				$select_products = mysqli_query($conn, "SELECT * FROM Products WHERE Name LIKE '%{$search_item}%'") or die('Query Unsuccessful!');
+
+				$search_item = test_input($_POST['search']);
+ 				$select_products = mysqli_query($conn, "SELECT * FROM Products WHERE Name LIKE '%{$search_item}%' OR Author LIKE '%{$search_item}%'") or die('Query Unsuccessful!');
  				if(mysqli_num_rows($select_products) > 0){
  					while($fetch_products = mysqli_fetch_assoc($select_products)){
  			?>
 
- 			<form method="post" class="box">
+ 			<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post" class="box">
 
  						<img class="image" src="img_uploaded/<?php echo $fetch_products['Image']; ?>" alt="">
- 						<div class="name"><?php echo $fetch_products['Name']; ?></div>	
+ 						<div class="name"><?php echo $fetch_products['Name']; ?></div>
+ 						<div class="name">By: <?php echo $fetch_products['Author']; ?></div>	
  						<div class="price">R <?php echo $fetch_products['Price']; ?></div>
  						<input class="qty" type="number" min="1" name="product_quantity" value="1">
 
  						<input type="hidden" name="product_name" value="<?php echo $fetch_products['Name']; ?>">
+
+ 						<input type="hidden" name="product_author" value="<?php echo $fetch_products['Author']; ?>">
 
  						<input type="hidden" name="product_price" value="<?php echo $fetch_products['Price']; ?>">
 
@@ -106,6 +191,7 @@
  			} else {
  				echo '<p class="empty">Begin search!</p>';
  			}
+	 		}
 
  			?>
 
