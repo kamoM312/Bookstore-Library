aa2a75db6855854c929f2bbdab8706c4e4496830
kamoM312 <kamogelomalema1@gmail..com>
2024-09-02 17:42:20 +0200

    admin_users.php: removed bootstrap css and js links

--- admin_users.php
+++ admin_users.php
@@ -29,7 +29,6 @@ if(isset($_GET['delete'])){
 
 	<!-- custom css -->
 	<link rel="stylesheet" href="css/admin_style.css">
-	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
 	<!-- Add icon library -->
  	<link rel="stylesheet" 
@@ -68,9 +67,6 @@ if(isset($_GET['delete'])){
 		</div>
 
 	</section>
-
-
-	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script> 
 
 	<script src="js/admin_script.js"></script>
 
