ef7a359036cc53d0e8ec97a692921f7f0642cc76
kamoM312 <kamogelomalema1@gmail..com>
2024-09-02 17:19:54 +0200

    admin_products.php: removed bootstrap css and js links

--- admin_products.php
+++ admin_products.php
@@ -109,12 +109,10 @@ if(isset($_POST['update_product'])){
 
 	<!-- custom css -->
 	<link rel="stylesheet" href="css/admin_style.css">
-	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
 	<!-- Add icon library -->
  	<link rel="stylesheet" 
- 	href=
- 	"https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css">
+ 	href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css">
 </head>
 <body>
 
@@ -278,28 +276,6 @@ if(isset($_POST['update_product'])){
 			?>
 
 		</section>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 
 		<script src="js/admin_script.js"></script>
 
