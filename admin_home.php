2d71c614c4cef87d152bcbe8a496f15fc3a9f11e
kamoM312 <kamogelomalema1@gmail..com>
2024-09-02 16:40:54 +0200

    admin_home.php: removed bootstrap css and js links

--- admin_home.php
+++ admin_home.php
@@ -21,7 +21,7 @@ if(!isset($admin_id)) {
 
 	<!-- custom css -->
 	<link rel="stylesheet" href="css/admin_style.css">
-	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
 
 	<!-- Add icon library -->
  	<link rel="stylesheet" 
@@ -153,7 +153,7 @@ if(!isset($admin_id)) {
 
 <!-- Admin dashboard section end -->
 
- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+ 
 <!-- including js file -->
 <script src="js/admin_script.js"></script>
 
