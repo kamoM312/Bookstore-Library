44e63d43b386f7e106a2e605ba9743e422abd378
kamoM312 <kamogelomalema1@gmail..com>
2024-09-03 10:45:33 +0200

    config.php: set to connect to online database

--- config.php
+++ config.php
@@ -1,5 +1,8 @@
 <?php
 
-$conn = mysqli_connect("127.0.0.1","root","theOPdude","shop_db") or die('connection failed');
+// server, user, password, database
+// $conn = mysqli_connect("127.0.0.1","root","theOPdude","shop_db") or die('connection failed');
+
+$conn = mysqli_connect("sql7.freesqldatabase.com","sql7729145","fYxjxKxtNe","sql7729145") or die('connection failed');
 
 ?>
